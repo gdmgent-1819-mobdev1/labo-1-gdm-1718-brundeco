f5a3bdde50e4f15d7d6d989cf01ad2ac38062311
Bruno De Coene <brunodecoene@Brunos-MacBook-Pro-2.local>
2018-10-27 19:29:22 +0200

    Parking Update

--- Parking/script.js
+++ Parking/script.js
@@ -1,91 +1,68 @@
-// declaratie van de overview
-const parkingOverview = document.getElementById('parking-overview');
+let url = 'https://datatank.stad.gent/4/mobiliteit/bezettingparkingsrealtime.json#';
+let available = [];
+let total = [];
+let percent;
 
-// functie expressie om JSON-request via url uit te voeren
-const getJSON = function(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status, xhr.response);
-      }
-    };
-    xhr.send();
-};
 
-// get json data
-getJSON('https://datatank.stad.gent/4/mobiliteit/bezettingparkingsrealtime.json', function(error, data) {
+function fetchParkingData() {
+    fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
     
-    // show error
-    if(error) {
-        
-        // do something here
-        return false;
-    }
-
-    // loop through all parking places
-    for(let i = 0; i < data.length; i++) {
-
-        // current parking object
-        let parking = data[i];
-
-        // do magic here
-        let mainDiv = document.getElementById('container');
-
-        let parkingOverview = document.createElement('div');
-        parkingOverview.className = 'parkingOverview';
-        mainDiv.appendChild(parkingOverview);
-
-        let parkingInfo = document.createElement('div');
-        parkingInfo.className = document.createElement('parkingInfo');
-        parkingOverview.appendChild(parkingInfo);
-
-        let parkingName = document.createElement('h2');
-        parkingName.className = 'parkingName';
-        parkingName.innerHTML = parking.name;
-        parkingInfo.appendChild(parkingName);
-
-        let parkingList = document.createElement('ul');
-        parkingList.className = 'parkingList';
-        parkingInfo.appendChild(parkingList);
-
-        let parkingCapacity = document.createElement('li');
-        parkingCapacity.className = 'parkingCapacity';
-        parkingCapacity.innerHTML = 'Aantal plaatsen: ' + parking.parkingStatus.totalCapacity;
-        parkingList.appendChild(parkingCapacity);
-
-        let parkingAvailable = document.createElement('li');
-        parkingAvailable.className = 'parkingAvailable';
-        parkingAvailable.innerHTML = 'Momenteel beschikbaar: ' +parking.parkingStatus.availableCapacity;
-        parkingList.appendChild(parkingAvailable);
+        for(let i = 0; i < data.length; i++) {
+            let parkingData = {
+                name: data[i].name,
+                totalCapacity: data[i].parkingStatus.totalCapacity,
+                availableCapacity: data[i].parkingStatus.availableCapacity,
+            }
+    
+            let mainDiv = document.getElementById('mainDiv');
+            let parkingDiv = document.createElement('div');
+            parkingDiv.className = 'parking-div';
+            let name = document.createElement('h2');
+            let totalCapacity = document.createElement('h3');
+            let availableCapacity = document.createElement('h4');
+            let indicator = document.createElement('p');
+            
+            name.innerHTML = parkingData.name;
+            totalCapacity.innerHTML = 'Capaciteit: ' + parkingData.totalCapacity;
+            availableCapacity.innerHTML = 'Beschikbaar: ' + parkingData.availableCapacity;
+            if(availableCapacity )
+    
+            mainDiv.appendChild(parkingDiv);
+            parkingDiv.appendChild(name);
+            parkingDiv.appendChild(totalCapacity);
+            parkingDiv.appendChild(availableCapacity);
+            
+            percent =  Math.floor(parkingData.availableCapacity / parkingData.totalCapacity * 100);
+            console.log(percent);
+    
+            if(percent < 20) {
+                parkingDiv.style.backgroundColor = 'red';
+            } if(percent >= 20 && percent <= 50) {
+                parkingDiv.style.backgroundColor = 'orange';
+            } if(percent > 50) {
+                parkingDiv.style.backgroundColor = 'green';
+            }
 
 
+        };
+    
+    })
+    .catch(function(error) {
+        // error handling
+        parkingDiv.innerHTML = 'Data could not be fetched';
+    });
+}
 
+fetchParkingData();
 
-
-
-
-
-
-
-
+// setTimeout(function(){
+//     window.location.reload(1);
+//  }, 5000);
 
 
 
 
 
-
-
-
-
-
-
-
-
-    }
-});
 
